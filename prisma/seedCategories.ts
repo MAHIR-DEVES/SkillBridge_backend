5bcb9734da7373add67fc6a07fba75067919190d
Md Meherab Hossain Munna <mdmeherab657@gamil.com>
2026-02-20 15:08:31 +0600

    fix:backend url change

--- prisma/seedCategories.ts
+++ prisma/seedCategories.ts
@@ -1,12 +1,26 @@
-import { prisma } from "../src/lib/prisma";
-
+import { prisma } from '../src/lib/prisma';
 
 const categories = [
-  "Accounting", "Biology", "Chemistry", "Data Science",
-  "English", "Finance", "Geography", "History",
-  "IT & Software", "JavaScript", "Korean Language", "Literature",
-  "Math", "Networking", "Physics", "Programming",
-  "Robotics", "Spanish Language", "Web Development", "Writing"
+  'Accounting',
+  'Biology',
+  'Chemistry',
+  'Data Science',
+  'English',
+  'Finance',
+  'Geography',
+  'History',
+  'IT & Software',
+  'JavaScript',
+  'Korean Language',
+  'Literature',
+  'Math',
+  'Networking',
+  'Physics',
+  'Programming',
+  'Robotics',
+  'Spanish Language',
+  'Web Development',
+  'Writing',
 ];
 
 async function main() {
@@ -17,9 +31,9 @@ async function main() {
       create: { name },
     });
   }
-  console.log("Categories seeded successfully!");
+  console.log('Categories seeded successfully!');
 }
 
 main()
-  .catch((e) => console.error(e))
+  .catch(e => console.error(e))
   .finally(async () => await prisma.$disconnect());
